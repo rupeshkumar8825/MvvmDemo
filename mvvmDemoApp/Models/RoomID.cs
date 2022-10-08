bce50754898902a7f4ed73ef13050681ff8532f7
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2022-10-08 10:39:18 +0530

    Completed the implementation of the classes of the models of this application

--- mvvmDemoApp/Models/RoomID.cs
+++ mvvmDemoApp/Models/RoomID.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Windows.Themes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,35 @@ namespace mvvmDemoApp.Models
 {
     public class RoomID
     {
+        public int FloorNumber { get; }
+        public int RoomNumber { get; }
+
+        //defining the constructor for this purpose 
+        public RoomID(int floorNumber, int roomNumber)
+        {
+            FloorNumber = floorNumber;
+            RoomNumber = roomNumber;
+        }
+
+
+        //overriding the equal method so that dictionaries knows how to comapre these keys 
+        public override bool Equals(object obj)
+        {
+            return (obj is RoomID roomID &&  
+                FloorNumber == roomID.FloorNumber &&
+                RoomNumber == roomID.RoomNumber);
+        }
+
+        //since we are overriding the equal then we also have to set the hashcode 
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FloorNumber, RoomNumber);
+        }
+
+        //we can also override the to string question for this class 
+        public override string ToString()
+        {
+            return $"{FloorNumber} {RoomNumber}"; 
+        }
     }
 }
